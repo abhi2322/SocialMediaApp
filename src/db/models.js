925be3a9ed186360a9ae29f040bf732e72e4ebfd
abhi2322 <abhishekjamwal34@gmail.com>
2021-05-11 23:53:22 +0530

    Added models

---
+++ src/db/models.js
@@ -0,0 +1,56 @@
+const {Sequelize, DataTypes, DATE}=require('sequelize')
+const db =new Sequelize({
+    database:"socialmediadb",
+    username:"socialuser",
+    password:"socialpass",
+    dialect:"mysql"
+})
+const COL_ID_DEF={
+    type:DataTypes.INTEGER,
+    autoIncrement:true,
+    primaryKey:true
+    
+}
+
+const Users=db.define('user',{
+    id:COL_ID_DEF,
+    username:{
+        type:DataTypes.STRING,
+        allowNull:false
+    }
+})
+
+const Posts= db.define('post',{
+    id:COL_ID_DEF,
+    title:{
+        type:DataTypes.STRING(140),
+        allowNull:false
+    },
+    body:{
+        type:DataTypes.TEXT,
+        allowNull:false
+    }
+})
+
+const Comments=db.define('commnet',{
+    id:COL_ID_DEF,
+    title:{
+        type:DataTypes.STRING(140),
+        allowNull:false
+    },
+    body:{
+        type:DataTypes.TEXT('tiny'),
+    }
+})
+
+Users.hasMany(Posts)
+Posts.belongsTo(Users)
+Users.hasMany(Comments)
+Comments.belongsTo(Users)
+Posts.hasMany(Comments)
+Comments.belongsTo(Posts)
+
+
+module.exports={
+    db,Users,Posts,Comments
+}
---
+++ src/server.js
@@ -0,0 +1,18 @@
+const express=require('express')
+
+
+const {db}=require('./db/models')
+const app=express()
+
+db.sync({force:true})
+.then(()=>{
+    app.listen(3456,()=>{
+        console.log('server started at http://localhost:3456')
+    })
+})
+.catch((err)=>{
+    console.error(new Error('Could not start database'))
+    console.error(err)
+})
+
+
